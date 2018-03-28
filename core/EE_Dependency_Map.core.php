2bf77e82f1662e54bc2493912f22376dcb5db802
Brent Christensen <brent@eventespresso.com>
2018-03-28 12:56:33 -0700

    update dependencies; [touch:11467]

--- core/EE_Dependency_Map.core.php
+++ core/EE_Dependency_Map.core.php
@@ -680,9 +680,10 @@ class EE_Dependency_Map
                 'EE_Request'                                                      => EE_Dependency_Map::load_from_cache,
                 'EventEspresso\core\domain\Domain'                                => EE_Dependency_Map::load_from_cache,
             ),
-            'EventEspresso\core\services\editor\EditorBlockAdminManager'      => array(
-                'EE_Request'                       => EE_Dependency_Map::load_from_cache,
-                'EventEspresso\core\domain\Domain' => EE_Dependency_Map::load_from_cache,
+            'EventEspresso\core\services\editor\EspressoGutenbergEditor' => array(
+                'EventEspresso\core\domain\entities\editor\EditorBlockCollection' => EE_Dependency_Map::load_from_cache,
+                'EE_Request'                                                      => EE_Dependency_Map::load_from_cache,
+                'EventEspresso\core\domain\Domain'                                => EE_Dependency_Map::load_from_cache,
             ),
             'EventEspresso\core\domain\entities\editor\blocks\shortcodes\EventsList'     => array(
                 'EventEspresso\core\domain\Domain' => EE_Dependency_Map::load_from_cache,
